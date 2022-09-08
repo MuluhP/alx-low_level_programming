6cc110a8d3f8490ff0f173df88159a4efe66c76c
MuluhP <muluhprecious@gmail.com>
2022-09-08 16:54:00 -0700

    fix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-/*
- *  main function
+/**
+ *  main - function prints a line using puts
  *  Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
