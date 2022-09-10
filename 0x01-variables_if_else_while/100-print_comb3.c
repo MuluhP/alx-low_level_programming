35bf59b18168fa0650e45c4d2638dcec39054a24
MuluhP <muluhprecious@gmail.com>
2022-09-09 20:08:13 -0700

    numbers edited

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n <= 51; n++)
 	{
-		for (m = 48; m <= 57; m++)
+		for (m = 48; m <= 51; m++)
 		{
 			putchar(n);
 			putchar(m);
-			if (n != 57 || m != 57)
+			if (n != 51 || m != 51)
 			{
 				putchar(',');
 				putchar(' ');
