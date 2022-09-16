267d2fa0e13166f88e0dea2d656c6cf1f5883958
MuluhP <muluhprecious@gmail.com>
2022-09-16 06:50:11 -0700

    errors corrected

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,6 @@
 /**
  *print_numbers - prints the numbers, 0 to 9
  * followed by a new line
- * Returns: nothing
  */
 
 void print_numbers(void)
@@ -14,8 +13,5 @@ void print_numbers(void)
 	{
 		_putchar(i + '0');
 	}
-
 	_putchar('\n');
-
-	return (nothing);
 }
