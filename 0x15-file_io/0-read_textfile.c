22d0c0e9c58ef3b2492562043e05c6fd890d3bbf
MuluhP <muluhprecious@gmail.com>
2022-11-02 01:00:58 +0100

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,44 @@
 #include "main.h"
-  2
-  3 /**
-  4  * read_textfile - read a certain size and prints to std output
-  5  * @filename: file to read from
-  6  * @letters: size to read
-  7  * Return: actual size read and printed
-  8  */
-  9
- 10 ssize_t read_textfile(const char *filename, size_t letters)
- 11 {
- 12         int fd; /* file descriptor */
- 13         ssize_t n_read, n_wrote;
- 14         char *buffer;
- 15
- 16         if (filename == NULL)
- 17                 return (0);
- 18
- 19         /* open */
- 20         fd = open(filename, O_RDONLY);
- 21         if (fd == -1)
- 22                 return (0);
- 23
- 24         /* malloc buffer */
- 25         buffer = malloc(sizeof(char) * letters);
- 26         if (buffer == NULL)
- 27                 return (0);
- 28
- 29         /* read */
- 30         n_read = read(fd, buffer, letters);
- 31         if (n_read == -1)
- 32         {
- 33                 free(buffer);
- 34                 close(fd);
- 35                 return (0);
- 36         }
- 37
- 38         /* write */
- 39         n_wrote = write(STDOUT_FILENO, buffer, n_read);
- 40         if (n_wrote == -1)
- 41         {
- 42                 free(buffer);
- 43                 close(fd);
- 44                 return (0);
- 45         }
- 46
- 47         close(fd);
- 48         return (n_read);
- 49
- 50 }
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/**
+ * read_textfile - reads a certain amount of letters from a file
+ * @filename: name of file
+ * @letters: how many letters to read
+ * Return: the number of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	/* declarations */
+	int fd, retVal, writeVal;
+	char *buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL || filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	retVal = read(fd, buffer, letters);
+	if (retVal < 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	writeVal = write(STDOUT_FILENO, buffer, retVal);
+	if (writeVal < 0 || writeVal != retVal)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (retVal);
+}
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,52 @@
 #include "main.h"
 
 /**
- * __exit - prints error messages and exits with exit value
- * @error: num is either exit value or file descriptor
- * @s: str is a name, either of the two filenames
- * @fd: file descriptor
- * Return: 0 on success
- **/
-int __exit(int error, char *s, int fd)
-{
-	switch (error)
-	{
-	case 97:
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(error);
-	case 98:
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
-		exit(error);
-	case 99:
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-		exit(error);
-	case 100:
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(error);
-	default:
-		return (0);
-	}
-}
-
-/**
- * main - copies one file to another
- * @argc: should be 3 (./a.out copyfromfile copytofile)
- * @argv: first is file to copy from (fd_1), second is file to copy to (fd_2)
- * Return: 0 (success), 97-100 (exit value errors)
+ * main - copies the content of a file to another file
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int fd_1, fd_2, n_read, n_wrote;
-	char *buffer[1024];
+	int fd_r, fd_w, x, m, n;
+	char buf[BUFSIZ];
 
 	if (argc != 3)
-		__exit(97, NULL, 0);
-
-	/*sets file descriptor for copy-to file*/
-	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd_2 == -1)
-		__exit(99, argv[2], 0);
-
-	/*sets file descriptor for copy-from file*/
-	fd_1 = open(argv[1], O_RDONLY);
-	if (fd_1 == -1)
-		__exit(98, argv[1], 0);
-
-	/*reads original file as long as there's more than 0 to read*/
-	/*copies/writes contents into new file */
-	while ((n_read = read(fd_1, buffer, 1024)) != 0)
 	{
-		if (n_read == -1)
-			__exit(98, argv[1], 0);
-
-		n_wrote = write(fd_2, buffer, n_read);
-		if (n_wrote == -1)
-			__exit(99, argv[2], 0);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-
-	close(fd_2) == -1 ? (__exit(100, NULL, fd_2)) : close(fd_2);
-	close(fd_1) == -1 ? (__exit(100, NULL, fd_1)) : close(fd_1);
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
+	{
+		if (fd_w < 0 || write(fd_w, buf, x) != x)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_r);
+			exit(99);
+		}
+	}
+	if (x < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	m = close(fd_r);
+	n = close(fd_w);
+	if (m < 0 || n < 0)
+	{
+		if (m < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+		if (n < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+		exit(100);
+	}
 	return (0);
 }
